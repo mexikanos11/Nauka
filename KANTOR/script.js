da59f8772a42f1e2dc6fe165509d7a57271d41d4
User <bartosz1293@gmail.com>
2021-10-01 10:55:23 +0200

    initial commit

---
+++ KANTOR/script.js
@@ -0,0 +1,33 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-zl");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+
+let rateEUR = 4.42;
+let rateDOLAR = 3.90;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "DOLAR":
+            result = amount / rateDOLAR;
+            break;
+    }
+
+
+    resultElement.innerHTML = `${result}`;
+
+});
